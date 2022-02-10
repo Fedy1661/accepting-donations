0010aa66bb252ced2d69d2944872cacaca54cc54
Fedor Konovalov <f.konovalov@ylab.io>
2022-02-10 13:01:57 +0300

    Add debug

--- task/TaskController.js
+++ task/TaskController.js
@@ -28,6 +28,7 @@ class TaskController {
 		const donate = await Donate.attach(from)
 
 		const tx = await donate.connect(owner).withdraw(to)
+		console.log('In progress')
 
 		await tx.wait()
 		console.log('Withdrawn');
@@ -41,7 +42,10 @@ class TaskController {
 		const donate = await Donate.attach(to)
 
 		const tx = await donate.connect(owner).deposit({value})
+		console.log('In progress');
+
 		await tx.wait()
+		console.log('Deposited');
 
 		const ownerBalance = (await owner.getBalance()).toString()
 		const contractBalance = (await web3.eth.getBalance(to)).toString()
