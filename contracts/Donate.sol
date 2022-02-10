93cc91ac4b5a08b90d8e0153aed6ff6382c03771
Fedor Konovalov <f.konovalov@ylab.io>
2022-02-10 07:49:53 +0300

    Make variable OWNER internal

--- contracts/Donate.sol
+++ contracts/Donate.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Donate {
-    address public owner;
+    address owner;
     mapping(address => uint) public payments;
 
     constructor(){
