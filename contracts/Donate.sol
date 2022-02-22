40fc61bc5001c71a59d167e2fc10d2e5dc06a8e1
Fedor Konovalov <f.konovalov@ylab.io>
2022-02-22 11:47:44 +0300

    Rename payments to senderStructs Add array with senders address Add condition in deposit function Add _value parameter to withdraw function Add getTotalSumOfSender function returns total sum of the current sender Add getSenders function returns all senders

--- contracts/Donate.sol
+++ contracts/Donate.sol
@@ -6,19 +6,31 @@ import "hardhat/console.sol";
 
 contract Donate {
     address owner;
-    mapping(address => uint) public payments;
+    mapping(address => uint) private senderStructs;
+    address [] private senderList;
 
     constructor(){
         owner = msg.sender;
     }
 
     function deposit() external payable {
-        payments[msg.sender] = msg.value;
+        if(senderStructs[msg.sender] == 0 && msg.value > 0) {
+            senderList.push(msg.sender);
+        }
+
+        senderStructs[msg.sender] += msg.value;
     }
 
-    function withdraw(address payable _to) external {
+    function withdraw(uint _value, address payable _to) external {
         require(msg.sender == owner, 'You should be an owner');
-        address thisContract = address(this);
-        _to.transfer(thisContract.balance);
+        _to.transfer(_value);
+    }
+
+    function getTotalSumOfSender(address _address) external view returns(uint) {
+        return senderStructs[_address];
+    }
+
+    function getSenders() external view returns( address  [] memory){
+        return senderList;
     }
 }
