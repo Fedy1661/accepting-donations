02b0481edfbf9d5fbc5f8cab410bf0ca3529b2ec
Fedor Konovalov <f.konovalov@ylab.io>
2022-03-05 08:10:21 +0300

    Add const for process.env.RINKEBY_PRIVATE_KEY, process.env.ALCHEMY_API_URL

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,14 +7,18 @@ require('dotenv').config();
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
 module.exports = {
   defaultNetwork: 'hardhat',
   solidity: '0.8.4',
   networks: {
     hardhat: {},
     rinkeby: {
-      url: process.env.ALCHEMY_API_URL,
-      accounts: process.env.RINKEBY_PRIVATE_KEY ? [process.env.RINKEBY_PRIVATE_KEY] : []
+      url: ALCHEMY_API_URL,
+      accounts: RINKEBY_PRIVATE_KEY !== undefined ? [RINKEBY_PRIVATE_KEY] : []
     }
   }
 };
